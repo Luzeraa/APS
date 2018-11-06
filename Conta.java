fb127de73636d1ab51093b95435018c56ecbfd87
Lucas Mendes da Silva <44248971+Luzeraa@users.noreply.github.com>
2018-11-06 18:12:41 -0200

    Conta Corrente e Conta terminados

--- Conta.java
+++ Conta.java
@@ -1,30 +1,25 @@
-
-package conta;
-
+package banco;
 
 public abstract class Conta {
+
     private String numeroConta;
     private String nomeCliente;
     private String cpf;
-    private double saldo;
+    private float saldo;
 
-    
-    public Conta(){
+    public Conta() {
         this.numeroConta = "Nao informado";
         this.nomeCliente = "Nao informado";
         this.cpf = "Nao informado";
-        
-    
     }
-    
+
     public Conta(String numeroConta, String nomeCliente, String cpf, double saldo) {
         this.numeroConta = numeroConta;
         this.nomeCliente = nomeCliente;
         this.cpf = cpf;
-        
     }
 
-    public String getNumeroConta() {
+    public String getNumeroConta(){
         return numeroConta;
     }
 
@@ -52,34 +47,30 @@ public abstract class Conta {
         this.cpf = cpf;
     }
 
-    private void setSaldo(double saldo) {
+    private void setSaldo(float saldo) {
         this.saldo = saldo;
     }
-    
-    public boolean sacar(double valor){ 
-       if((saldo - valor) >= 0){
-           saldo -= valor;
-           return true;
-        }else{ 
-            return false;
-        }     
-    }
-    
-    public boolean depositar(double valor){
-        if(valor >= 0){
-            saldo += valor;
+
+    public boolean sacar(float valor) {
+        if ((saldo - valor) >= 0) {
+            saldo -= valor;
             return true;
-        }else{
+        } else {
             return false;
         }
     }
-        
-   
-    public String imprimir(){
-        return "Titular: " + this.nomeCliente + "Nº conta: " + this.numeroConta +"\n"
-        + "CPF: " + this.cpf + "Saldo: " + this.saldo;
+
+    public boolean depositar(float valor) {
+        if (valor >= 0) {
+            saldo += valor;
+            return true;
+        } else {
+            return false;
+        }
     }
-    
-    
-          
+
+    public String imprimir() {
+        return "Titular: " + nomeCliente + "Nº conta: " + numeroConta + "\n"
+                + "CPF: " + cpf + "Saldo: " + saldo;
+    }
 }
--- ContaCorrente.java
+++ ContaCorrente.java
@@ -1,16 +1,52 @@
+package banco;
 
-package conta;
+public class ContaCorrente extends Conta {
 
-public class ContaCorrente extends Conta{
     private double limite;
+
+    public ContaCorrente(String numeroConta, String nomeCliente, String cpf,
+                         double saldo) {
+        super(numeroConta, nomeCliente, cpf, saldo);      
+    }
     
     public ContaCorrente(String numeroConta, String nomeCliente, String cpf,
-            double saldo, float limite){
+                         double saldo, float limite) {
         super(numeroConta, nomeCliente, cpf, saldo);
         this.limite = limite;
     }
     
-    public boolean usandoLimite(float valor){
-        if()
+    
+    @Override
+    public boolean sacar(float valor){ 
+       if((valor <= (super.getSaldo()+ limite) ) ){
+            super.sacar(valor);
+            return true;
+       }else{
+           return false;
+       }
     }
+       
+    
+    @Override
+    public String imprimir(){
+        return "Titular: " + getNomeCliente() + "Nº conta: " + getNumeroConta() +"\n"
+        + "CPF: " + getCpf() + "Saldo: " + getSaldo() + "Limite: " + limite;
+    }
+
+    
+    @Override
+    public String getNumeroConta() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+    public boolean usandoLimite(float saque){
+        if(sacar(saque) == true){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
 }
